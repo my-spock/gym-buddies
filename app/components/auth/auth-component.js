fa9ef8a62986ae53891477ae31db8ba1d870534c
Jake Overall <joverall22@gmail.com>
2016-05-10 07:26:08 -0600

    fixes login

--- app/components/auth/auth-component.js
+++ app/components/auth/auth-component.js
@@ -5,96 +5,82 @@
 			templateUrl: 'app/components/auth/auth.html',
 			controller: AuthController
 		})
-		.service('AuthService', AuthService)
 
-
-	function AuthController(AuthService) {
+	function AuthController($scope) {
 		var ac = this;
+		var db = new Firebase('https://gym-buddies.firebaseio.com/');
 
 		ac.$onInit = activate;
 
+		function update(snapshot) {
+			if (snapshot) {
+				ac.member = snapshot.val();
+			}
+			$scope.$evalAsync(function () {
+				ac = ac;
+			})
+		}
+
 		function activate() {
-			AuthService.getAuth();
-			ac.member = AuthService.member;
+			getAuth();
 		}
 
 		ac.login = function () {
 			clearError()
-			AuthService.login(ac.auth).catch(handleError).then(function(res){
-				if(res.message){
-					ac.error = res.message;
-				}
-			})
+			db.authWithPassword(ac.auth).catch(handleError).then(getAuth)
 		}
 
 		ac.register = function () {
 			clearError()
-			AuthService.register(ac.auth).catch(handleError).then(function(res){
-				if(res.message){
-					ac.error = res.message;
-				}
-			})
+			db.createUser(ac.auth).catch(handleError).then(registerMember)
+		}
+
+
+		function getAuth() {
+			var authData = db.getAuth()
+			if (authData) {
+				ac.userRef = db.child('users').child(authData.uid);
+				ac.userRef.on('value', update)
+				closeModal()
+			} else {
+				//show modal
+				showModal()
+			}
+		}
+		
+		function showModal(){
+			ac.activeView = 'login'
+			update()
+		}
+		
+		function closeModal(){
+			ac.activeView = ''
+			update()
+		}
+
+		function registerMember(authData) {
+			if (authData.error) {
+				return authData.error;
+			}
+			ac.auth.authData = authData;
+			db.child('users').child(authData.uid).set(ac.auth)
+			ac.login()
 		}
 
 		function handleError(err) {
-			console.log(err)
+			ac.err = err.message
 			return err;
 		}
 
 		function clearError() {
 			ac.error = null;
 		}
+
+		ac.logout = function () {
+			ac.userRef.off('value', update);
+			ac.member = null;
+			db.unauth();
+		}
+
 	}
-
-
-	function AuthService() {
-		var db = new Firebase('https://gym-buddies.firebaseio.com/');
-		var authService = this;
-		authService.member = {}
-
-		this.getAuth = function () {
-			var authData = db.getAuth()
-			if (authData) {
-				var userRef = db.child('users').child(authData.uid);
-				userRef.on('value', setMember)
-			}
-		}
-		
-		function handleError(err) {
-			console.log(err)
-			return err;
-		}
-
-		this.login = function (user, errorHandler) {
-			return db.authWithPassword(user).catch(errorHandler).then(function (authData) {
-				if(authData.error){
-					return authData.error;
-				}
-				authService.getAuth();
-			})
-		}
-
-		this.register = function (user, errorHandler) {
-			return db.createUser(user).catch(errorHandler).then(function (authData) {
-				if(authData.error){
-					return authData.error;
-				}
-				user.authData = authData;
-				db.child('users').child(authData.uid).set(user)
-				authService.login(user)
-			})
-		}
-
-		this.logout = function () { authService.member.off('value', setMember); db.unauth(); authService.member = {} }
-
-		function setMember(snapshot) {
-			var memberData = snapshot.val();
-			if (memberData) {
-				Object.keys(memberData).forEach(function (prop) {
-					authService.member[prop] = memberData[prop];
-				})
-			}
-		}
-	}
-
 } ())
